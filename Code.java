ba785f10eaf67dcce8f05012d2227589c007439e
2NyAvo1514 <nyavo281@gmail.com>
2025-09-24 18:53:36 +0300

    commit 2 from sprint1

--- Code.java
+++ Code.java
@@ -4,4 +4,8 @@ public class Code {
     public static String codeString(){
         return "Votre URL est passee par ici (Framework fait maison) !";
     }
+    @Arabe(cheminWeb = "/andrana",method = "GET")
+    public static String testAnnotationRoute() {
+        return "Nety ilay andrana .";
+    }
 }
